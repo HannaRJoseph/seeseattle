366e49e6805f05137e936c676acf65a3df3330af
Sofiya Mitchell <sofiya.mitchell@gmail.com>
2019-08-14 10:39:40 -0700

    finished nav bar and js file

--- SofiyaAttractions.js
+++ SofiyaAttractions.js
@@ -1,12 +1,12 @@
 function foodclick(){
-  window.location.href=""
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/hannahfood.html"
 }
 function shoppingclick(){
-
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/SinitShopping.html"
 }
 function musicclick(){
-
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/kristyart.html"
 }
 function todoclick(){
-
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/SofiyaAttractions.html"
 }
