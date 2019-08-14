5766891973263b55404cbce34d0c0afe10ae7bf3
Sofiya Mitchell <sofiya.mitchell@gmail.com>
2019-08-14 11:11:04 -0700

    nav bar on all pages

--- hannahsee.js
+++ hannahsee.js
@@ -1,3 +1,12 @@
-<script>
-<alert(“Hello World!”)
-<script>
+function foodclick(){
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/hannahfood.html"
+}
+function shoppingclick(){
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/SinitShopping.html"
+}
+function musicclick(){
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/kristyart.html"
+}
+function todoclick(){
+  location.href="file:///C:/Users/girlswhocode/Desktop/seeseattle/SofiyaAttractions.html"
+}
